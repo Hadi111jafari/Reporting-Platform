c94c0b501aeac1309d128b091169357e174eca30
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-02 04:07:44 -0300

    chore: cover options

---
+++ app/_shared/coverOptions.ts
@@ -0,0 +1,16 @@
+const images = [
+  {
+    imageURL: 'https://img.freepik.com/fotos-gratis/fundo-fluido-gradiente-3d_23-2150499051.jpg?w=1060'
+  },
+  {
+    imageURL: 'https://img.freepik.com/vetores-gratis/fundo-realista-com-dobras-em-3d_23-2149146617.jpg?w=1060'
+  },
+  {
+    imageURL: 'https://img.freepik.com/fotos-gratis/fundo-fluido-gradiente-3d_23-2150499065.jpg?w=1060'
+  },
+  {
+    imageURL: 'https://img.freepik.com/fotos-gratis/fundo-fluido-gradiente-3d_23-2150499053.jpg?&w=1060'
+  },
+]
+
+export default images
