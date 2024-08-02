409a272d4ccee29c50a5a9becbb9ba329b33a480
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-02 03:07:21 -0300

    feature: redirect to dashboard when create/leave organization

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,12 +1,20 @@
+"use client";
 import LogoComponent from "@/app/_components/Logo";
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, useAuth, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const HeaderComponent = () => {
+  const { orgId } = useAuth();
+  console.info({
+    orgId,
+  });
   return (
     <div className="flex justify-between items-center p-3 shadow-sm">
       <LogoComponent />
-      <OrganizationSwitcher />
+      <OrganizationSwitcher
+        afterCreateOrganizationUrl={"/dashboard"}
+        afterLeaveOrganizationUrl="/dashboard"
+      />
       <UserButton />
     </div>
   );
