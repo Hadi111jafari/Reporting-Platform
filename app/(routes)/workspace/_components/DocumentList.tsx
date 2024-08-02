5a8188a9d196bdad31cd2a617d9f86377a3f925e
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-02 18:08:58 -0300

    feature: create document list component

---
+++ app/(routes)/workspace/_components/DocumentList.tsx
@@ -0,0 +1,41 @@
+import { DocumentData } from "firebase/firestore";
+import { FileClockIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const DocumentListComponent = ({
+  documentList,
+  params,
+}: {
+  documentList: DocumentData[];
+  params: { workspaceId: string; documentId: string };
+}) => {
+  const router = useRouter();
+  return (
+    <div>
+      {documentList.map((doc, index) => {
+        return (
+          <div
+            onClick={() =>
+              router.push(`/workspace/${params.workspaceId}/${doc.id}`)
+            }
+            key={index}
+            className={`mt-3 p-2 px-2 hover:bg-gray-200 rounded-lg cursor-pointer ${
+              doc.id === params.documentId ? "bg-white" : ""
+            }`}
+          >
+            <div className="flex gap-2 items-center">
+              {!doc.emoji && <FileClockIcon className="text-primary" />}
+              <h2 className="flex gap-2">
+                {doc?.emoji}
+                {doc.documentName}
+              </h2>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default DocumentListComponent;
