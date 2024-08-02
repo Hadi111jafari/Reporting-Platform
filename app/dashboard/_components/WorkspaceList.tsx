4369f8dc1f6e94d2c36ea89b1ee4bd89a7ddce6e
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-02 03:27:10 -0300

    feature: render conditionally create workspace

--- app/dashboard/_components/WorkspaceList.tsx
+++ app/dashboard/_components/WorkspaceList.tsx
@@ -2,10 +2,13 @@
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/nextjs";
 import { AlignLeft, LayoutGrid } from "lucide-react";
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const WorkspaceListComponent = () => {
   const { user } = useUser();
+  const [workspaceList, setWorkspaceList] = useState([]);
+
   return (
     <div className="my-10 p-10 md:px-24 lg:px-36 xl:px-52">
       <div className="flex justify-between items-center">
@@ -22,6 +25,21 @@ const WorkspaceListComponent = () => {
           <AlignLeft />
         </div>
       </div>
+
+      {workspaceList.length ? (
+        <div>Workspace List</div>
+      ) : (
+        <div className="flex flex-col justify-center items-center my-10 gap-2">
+          <Image
+            src={"/workspace.png"}
+            alt=""
+            width={200}
+            height={200}
+          />
+          <h2>Create new workspace</h2>
+          <Button variant={'outline'} className="">+ New Workspace</Button>
+        </div>
+      )}
     </div>
   );
 };
