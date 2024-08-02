721493d0f8041d2c70d996bc000a8074e14eeb3f
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-02 04:20:56 -0300

    feature: import cover picker

--- app/(routes)/workspace/new/page.tsx
+++ app/(routes)/workspace/new/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import CoverPickerComponent from "@/app/_components/CoverPicker";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SmilePlus } from "lucide-react";
@@ -12,20 +13,22 @@ const CreateWorkspace = () => {
     <div className="p-10 md:px-36 lg:px-64 xl:px-96 py-28">
       <div className="shadow-2xl rounded-xl">
         {/* Cover Image */}
-        <div className="relative group cursor-pointer">
-          <h2 className="hidden group-hover:flex absolute p-4 w-full h-full items-center justify-center">
-            Change Cover
-          </h2>
-          <div className="group-hover:opacity-70">
-            <Image
-              src={cover}
-              alt="cover"
-              width={400}
-              height={400}
-              className="w-full h-[150px] object-cover rounded-t-xl"
-            />
+        <CoverPickerComponent>
+          <div className="relative group cursor-pointer">
+            <h2 className="hidden group-hover:flex absolute p-4 w-full h-full items-center justify-center">
+              Change Cover
+            </h2>
+            <div className="group-hover:opacity-70">
+              <Image
+                src={cover}
+                alt="cover"
+                width={400}
+                height={400}
+                className="w-full h-[150px] object-cover rounded-t-xl"
+              />
+            </div>
           </div>
-        </div>
+        </CoverPickerComponent>
 
         {/* Input Section */}
         <div className="p-12">
