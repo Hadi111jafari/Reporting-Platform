a38cf449d8c76ea895a16e3dbcf904acb2193b28
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-02 03:02:49 -0300

    feature: import org switcher from clerk

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,11 +1,12 @@
 import LogoComponent from "@/app/_components/Logo";
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const HeaderComponent = () => {
   return (
     <div className="flex justify-between items-center p-3 shadow-sm">
       <LogoComponent />
+      <OrganizationSwitcher />
       <UserButton />
     </div>
   );
