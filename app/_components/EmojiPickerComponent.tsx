7d4c3b49d6ba93829dd4ba41b29c658e127940bc
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-02 04:45:02 -0300

    feature: create emoji picker component

---
+++ app/_components/EmojiPickerComponent.tsx
@@ -0,0 +1,31 @@
+import EmojiPicker, { EmojiStyle } from "emoji-picker-react";
+import React, { useState } from "react";
+
+const EmojiPickerComponent = ({
+  children,
+  setEmojiIcon,
+}: {
+  children: React.ReactNode;
+  setEmojiIcon: (emojiIcon: string) => void;
+}) => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  return (
+    <div>
+      <div onClick={() => setIsOpened(true)}>{children}</div>
+      {isOpened && (
+        <div className="absolute z-10">
+          <EmojiPicker
+            emojiStyle={EmojiStyle.TWITTER}
+            onEmojiClick={(event) => {
+              setEmojiIcon(event.emoji);
+              setIsOpened(false);
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EmojiPickerComponent;
