b2ffa4f555c8173aa1e3015e46172e2287b7a4c2
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-02 03:47:46 -0300

    feature: add cover section

--- app/(routes)/workspace/new/page.tsx
+++ app/(routes)/workspace/new/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const CreateWorkspace = () => {
+  const [cover, setCover] = useState("/cover.jpg");
   return (
-    <div>CreateWorkspace</div>
-  )
-}
+    <div className="p1- md:px-36 lg:px-52 xl:px-80 py-20">
+      <div>
+        {/* Cover Image */}
+        <div className="relative group cursor-pointer">
+          <h2 className="hidden group-hover:flex absolute p-4 w-full h-full items-center justify-center">
+            Change Cover
+          </h2>
+          <div className="group-hover:opacity-70">
+            <Image
+              src={cover}
+              alt="cover"
+              width={400}
+              height={400}
+              className="w-full h-[150px] object-cover rounded-t-xl"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CreateWorkspace
+export default CreateWorkspace;
