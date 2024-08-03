841078ec83e71d6c8399fd5fe21d583af693d9b9
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-02 22:53:05 -0300

    feature: update document info

--- app/(routes)/workspace/_components/DocumentInfo.tsx
+++ app/(routes)/workspace/_components/DocumentInfo.tsx
@@ -1,19 +1,60 @@
 import CoverPickerComponent from "@/app/_components/CoverPicker";
 import EmojiPickerComponent from "@/app/_components/EmojiPickerComponent";
+import { db } from "@/config/firebase";
+import { doc, DocumentData, getDoc, updateDoc } from "firebase/firestore";
 import { SmilePlusIcon } from "lucide-react";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { toast } from "sonner";
 
-const DocumentInfoComponent = () => {
+const DocumentInfoComponent = ({
+  params,
+}: {
+  params: { workspaceId: string; documentId: string };
+}) => {
   const [cover, setCover] = useState("/cover.jpg");
   const [emoji, setEmoji] = useState("");
+  const [documentInfo, setDocumentInfo] = useState<DocumentData>({});
+
+  const getDocumentInfo = useCallback(async () => {
+    // Fetch document info
+    const docRef = doc(db, "WorkspaceDocuments", params.documentId);
+    const snapshot = await getDoc(docRef);
+
+    if (snapshot.exists()) {
+      const data = snapshot.data();
+      setDocumentInfo(data);
+      setEmoji(data?.emoji);
+      setCover(data?.coverImage ?? "/cover.jpg");
+    }
+  }, [params]);
+
+  const updateDocumentInfo = async (key: string, value: any) => {
+    try {
+      const docRef = doc(db, "WorkspaceDocuments", params.documentId);
+      await updateDoc(docRef, {
+        [key]: value,
+      });
+      toast.success("Document info updated successfully");
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to update document info");
+    }
+  };
+
+  useEffect(() => {
+    getDocumentInfo();
+  }, [getDocumentInfo]);
 
   return (
     <div>
       {/* Cover */}
 
       <CoverPickerComponent
-        onHandleSelectedCover={(coverURL) => setCover(coverURL)}
+        onHandleSelectedCover={(coverURL) => {
+          setCover(coverURL);
+          updateDocumentInfo("coverImage", coverURL);
+        }}
       >
         <div className="relative group cursor-pointer">
           <h2 className="hidden group-hover:flex absolute p-4 w-full h-full items-center justify-center">
@@ -34,7 +75,10 @@ const DocumentInfoComponent = () => {
       {/* Emoji Picker */}
 
       <div className="absolute ml-10 mt-[-35px] cursor-pointer">
-        <EmojiPickerComponent setEmojiIcon={(v) => setEmoji(v)}>
+        <EmojiPickerComponent setEmojiIcon={(v) => {
+          setEmoji(v)
+          updateDocumentInfo("emoji", v)
+        }}>
           <div className="bg-[#ffffffb0] p-4 rounded-md">
             {emoji ? (
               <span className="text-5xl">{emoji}</span>
@@ -46,6 +90,15 @@ const DocumentInfoComponent = () => {
       </div>
 
       {/* File Name */}
+      <div className="mt-10 p-10">
+        <input
+          type="text"
+          placeholder="Untitled Document"
+          defaultValue={documentInfo?.documentName}
+          className="font-bold text-4xl outline-none"
+          onBlur={(e) => updateDocumentInfo("documentName", e.target.value)}
+        />
+      </div>
     </div>
   );
 };
