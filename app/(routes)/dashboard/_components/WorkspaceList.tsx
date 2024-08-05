2e3dfc9951666a661ac5d06c99af30476cad4c7a
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-05 15:08:58 -0300

    feature: get worskpaces and render component

--- app/(routes)/dashboard/_components/WorkspaceList.tsx
+++ app/(routes)/dashboard/_components/WorkspaceList.tsx
@@ -1,14 +1,51 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useUser } from "@clerk/nextjs";
+import { db } from "@/config/firebase";
+import { useAuth, useUser } from "@clerk/nextjs";
+import {
+  collection,
+  DocumentData,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import { AlignLeft, LayoutGrid } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import WorkspaceItemListComponent from "./WorkspaceItemList";
 
 const WorkspaceListComponent = () => {
   const { user } = useUser();
-  const [workspaceList, setWorkspaceList] = useState([]);
+  const { orgId } = useAuth();
+  const [workspaceList, setWorkspaceList] = useState<DocumentData[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const getWorkspaceList = useCallback(async () => {
+    try {
+      const q = query(
+        collection(db, "Workspaces"),
+        where(
+          "orgId",
+          "==",
+          orgId ? orgId : user?.primaryEmailAddress?.emailAddress
+        )
+      );
+      const snapshot = await getDocs(q);
+      const workspaces = snapshot.docs.map((doc) => doc.data());
+      setWorkspaceList(workspaces);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching workspaces:", error);
+      setLoading(false);
+    }
+  }, [orgId, user?.primaryEmailAddress?.emailAddress]);
+
+  useEffect(() => {
+    if (user) {
+      getWorkspaceList();
+    }
+  }, [getWorkspaceList, user]);
 
   return (
     <div className="my-10 p-10 md:px-24 lg:px-36 xl:px-52">
@@ -29,8 +66,12 @@ const WorkspaceListComponent = () => {
         </div>
       </div>
 
-      {workspaceList.length ? (
-        <div>Workspace List</div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : workspaceList.length ? (
+        <div>
+          <WorkspaceItemListComponent workspaceList={workspaceList} />
+        </div>
       ) : (
         <div className="flex flex-col justify-center items-center my-10 gap-2">
           <Image
