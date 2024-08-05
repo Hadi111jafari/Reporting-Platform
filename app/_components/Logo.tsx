45787890dc688744f007d273eeebdbbe7e71ce49
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-05 15:07:35 -0300

    chore: conver to a link

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const LogoComponent = () => {
   return (
-    <div className="flex items-center gap-2">
+    <Link className="flex items-center gap-2" href={'/dashboard'}>
       <Image
         src={"/logo.png"}
         width={30}
@@ -11,7 +12,7 @@ const LogoComponent = () => {
         alt=""
       />
       <h2 className="font-bold text-xl">Loop</h2>
-    </div>
+    </Link>
   );
 };
 
