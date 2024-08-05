c48d12c7b63c41753c77dcf8111573701875a202
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-05 18:53:45 -0300

    chore: redirect to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { useLayoutEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    router.replace("/dashboard");
+  }, [router]);
   return (
-    <div>
-      <h2>Teste aqui</h2>
-      <Button>
-        Teste
-      </Button>
+    <div className="flex justify-center items-center">
+      <h2>Landing Page</h2>
     </div>
   );
 }
