23203f33a6175c3659bcee7e51f438319b4b42ef
Felipe Paz <xxfelipepaz@gmail.com>
2024-08-05 15:17:24 -0300

    feature: register user on load header

--- app/(routes)/dashboard/_components/Header.tsx
+++ app/(routes)/dashboard/_components/Header.tsx
@@ -1,10 +1,36 @@
 "use client";
 import LogoComponent from "@/app/_components/Logo";
-import { OrganizationSwitcher, useAuth, UserButton } from "@clerk/nextjs";
-import React from "react";
+import { db } from "@/config/firebase";
+import {
+  OrganizationSwitcher,
+  useAuth,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+import { doc, setDoc } from "firebase/firestore";
+import React, { useCallback, useEffect } from "react";
+import uuid4 from "uuid4";
 
 const HeaderComponent = () => {
-  const { orgId } = useAuth();
+  const { user } = useUser();
+  const saveUserData = useCallback(async () => {
+    try {
+      if (user) {
+        const docId = user.primaryEmailAddress!.emailAddress;
+        await setDoc(doc(db, "Users", docId), {
+          name: user?.fullName,
+          avatar: user?.imageUrl,
+          email: user?.primaryEmailAddress?.emailAddress,
+        });
+      }
+    } catch (error) {
+      console.error("Error adding user: ", error);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    saveUserData();
+  }, [saveUserData]);
   return (
     <div className="flex justify-between items-center p-3 shadow-sm">
       <LogoComponent />
